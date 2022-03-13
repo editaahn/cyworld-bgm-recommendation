da99a47fbb31b398d100c83e91ad43002480d620
Rita <editaahn@yahoo.com>
2022-03-13 14:57:59 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: `new`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-X94BQZYX1Y"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
