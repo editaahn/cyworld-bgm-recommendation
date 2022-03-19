0d6d6d83327d18c92a8d1191b64e90d8edd6e67b
Rita <editaahn@yahoo.com>
2022-03-20 01:13:43 +0900

    add interactions when choosing an answer

--- src/components/AnswerList/AnswerList.tsx
+++ src/components/AnswerList/AnswerList.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import { Link } from 'gatsby';
+import { ScoreValue } from '../../contexts/QuestionContext';
 
 export type AnswerListProps = {
   answers: {
     value: string;
-    scoring: string[];
+    scoring: ScoreValue[];
   }[];
+  nextPath: string;
+  onClick: (values: ScoreValue[]) => void;
 };
 
-export const AnswerList = ({ answers }: AnswerListProps) => {
+export const AnswerList = ({ answers, nextPath, onClick }: AnswerListProps) => {
   return (
     <ol>
-      {answers.map(({ value }) => (
-        <li>{value}</li>
+      {answers.map(({ value, scoring }) => (
+        <Link key={value} to={nextPath}>
+          <li onClick={() => onClick(scoring)}>{value}</li>
+        </Link>
       ))}
     </ol>
   );
--- src/templates/QuestionPage.tsx
+++ src/templates/QuestionPage.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import { AnswerList, AnswerListProps } from '../components/AnswerList/AnswerList';
 import { graphql } from 'gatsby';
 import { Question } from '../components/Question';
 import styled from '@emotion/styled';
+import { useQuestionContext } from '../contexts/QuestionContext';
+import { useMemo } from 'react';
 
 // createPage의 context를 통해 pageNumber가 전달됨
 export const query = graphql`
@@ -14,6 +15,9 @@ export const query = graphql`
         value
       }
     }
+    sitePage {
+      path
+    }  
   }
 `;
 
@@ -22,17 +26,28 @@ type QuestionProps = {
     dataJson: {
       question: string;
     } & AnswerListProps;
+    sitePage: {
+      path: string;
+    }
   };
 };
 
 // query의 result가 data prop으로 전달됨
 const QuestionPage = ({ data }: QuestionProps) => {
   const { question, answers } = data.dataJson;
+  const { path } = data.sitePage;
+  const { addScores } = useQuestionContext();
+
+  const nextPath = useMemo(() => {
+    const splitedPath: (string | number)[] = path.split('/');
+    const nextStep = Number(splitedPath.pop()) + 1;
+    return splitedPath.concat(nextStep).join('/')
+  }, []);
 
   return (
     <Container>
       <Question>{question}</Question>
-      <AnswerList answers={answers} />
+      <AnswerList answers={answers} nextPath={nextPath} onClick={addScores} />
     </Container>
   );
 };
