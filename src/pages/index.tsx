8b4195232037263cb5d5db1ae52c1a7b110f1905
Rita <editaahn@yahoo.com>
2022-03-25 23:24:21 +0900

    make total process

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import { Link } from "gatsby";
+
 const IndexPage = () => {
   return (
     <main>
       <h1>싸이월드 BGM을 추천해드립니다</h1>
-      <button>Start!</button>
+      <Link to='/question/1'>Start!</Link>
     </main>
   );
 };
