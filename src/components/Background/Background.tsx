02349cf4d8c6e489006eee57bdea2a635f74f68a
Rita <41330862+editaahn@users.noreply.github.com>
2022-04-07 00:37:53 +0900

    style home (#18)

---
+++ src/components/Background/Background.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(#ff770f, #fff);
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,53 @@
 import styled from "@emotion/styled";
 import { Link } from "gatsby";
+import { Container } from "../components/Container";
+import '../../global.css';
+import { Background } from "../components/Background";
 
 const IndexPage = () => {
   return (
-    <Container>
-      <h1>싸이월드 BGM을 추천해드립니다</h1>
-      <Link to='/question/1'>Start!</Link>
-    </Container>
+    <Background>
+      <Container>
+        <TitleWrapper>
+          <Title>싸이월드 BGM을 추천해드립니다</Title>
+        </TitleWrapper>
+        <StartButton>
+          <Link to='/question/1'>Start!</Link>
+        </StartButton>
+      </Container>
+    </Background>
   );
 };
 
 export default IndexPage;
 
-const Container = styled.div`
+const TitleWrapper = styled.div`
+  max-width: 375px;
+  padding: 0 30px;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  word-break: keep-all;
+`;
+
+const StartButton = styled.div`
+  width: 200px;
+  height: 40px;
+  border-radius: 20px;
+  box-shadow: 1px 0px 10px 5px #fff9;
+  text-decoration: none;
   display: flex;
   align-items: center;
-  flex-direction: column;
-  width: 100%;
+  justify-content: center;
+  margin-top: 30px;
+
+  a {
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    line-height: 40px;
+    font-size: 18px;
+    font-weight: bold;
+  }
 `;
