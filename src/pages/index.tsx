e23b21af90599494f588e794489ba58bf521d495
Rita <editaahn@yahoo.com>
2022-04-06 23:58:14 +0900

    style index page's container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
+import styled from "@emotion/styled";
 import { Link } from "gatsby";
 
 const IndexPage = () => {
   return (
-    <main>
+    <Container>
       <h1>싸이월드 BGM을 추천해드립니다</h1>
       <Link to='/question/1'>Start!</Link>
-    </main>
+    </Container>
   );
 };
 
 export default IndexPage;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+`;
